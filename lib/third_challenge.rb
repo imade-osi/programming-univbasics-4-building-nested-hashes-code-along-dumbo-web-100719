63dadd54b899e211774ca90857ff63d5034d7ab8
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-09-26 22:49:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,11 +6,11 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        :name 
-        
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo"
         },
       :hero_friends => []
    }, 
